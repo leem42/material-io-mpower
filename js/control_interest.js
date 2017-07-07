663247bb810d4f0ec40e9eb6f0029711b0c1766b
Michael Lee <mtlee@michaels-mbp.fhcrc.org>
2017-07-07 11:04:14 -0700

    fixed broken link

--- js/control_interest.js
+++ js/control_interest.js
@@ -6,6 +6,11 @@
 
 function clicked() {
 
-    $("#optionBoxTwo").show();
+    if($("#select-choice-1").val() == "hasParkinsons") {
+        $("#optionBoxTwo").show();
+    }
+    if($("#select-choice-1").val() == "hasParkinsons" && $("#select-choice-2").val() == "willHelp") {
+        window.location.href="html/elig.html";
 
+    }
 }
